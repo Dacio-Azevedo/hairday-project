f27544c0e11e2decd3bf125d963329e2e15cde38
Dacio Azevedo <dacioherazan2@gmail.com>
2024-11-21 15:58:46 -0300

    Configurando o webpack server

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const CopyWebPackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
     target: "web",
@@ -8,5 +10,38 @@ module.exports = {
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, "dist"),
-    }
+    },
+
+    devServer: {
+        static: {
+            directory: path.join(__dirname, "dist")
+        },
+        port: 3000,
+        open: true,
+        liveReload: true, 
+    },
+
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: path.resolve(__dirname, "index.html"),
+            favicon: path.resolve("src", "assets", "scissors.svg"),
+        }),
+        new CopyWebPackPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, "src", "assets"),
+                    to: path.resolve(__dirname, "dist", "src", "assets"),
+                }
+            ]
+        })
+    ],
+
+    module : {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
 }
