575d1365b2901dce2bff8ce52d7a8fded45115e3
Dacio Azevedo <dacioherazan2@gmail.com>
2024-11-21 17:11:52 -0300

    Istalando o day.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 "use strict";
+//Configuranção Days.js
+import "./libs/dayjs.js";
 
 //CSS
 import "./styles/global.css";
 import "./styles/form.css";
 import "./styles/schedule.css";
+
+import dayjs from "dayjs";
+console.log(dayjs().format("HH:mm"));
